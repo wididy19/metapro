6be4504f9a95e2cad3847630b03d80bd46459fa2
Susilo Widiksono <widi1901@gmail.com>
2017-10-08 12:24:13 +0700

    first commit

--- src/pages/setortunai/setortunai.module.ts
+++ src/pages/setortunai/setortunai.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SetortunaiPage } from './setortunai';
+import { AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,9 @@ import { SetortunaiPage } from './setortunai';
   ],
   imports: [
     IonicPageModule.forChild(SetortunaiPage),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDfbk4cvPuRXj7iSEWPAPYEZtc-DUdlo-s'
+    })
   ],
 })
 export class SetortunaiPageModule {}
